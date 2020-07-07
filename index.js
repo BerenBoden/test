e2060b2a64a25535b4d979b1d5e61932fa07d93b
berenboden <beren.boden@gmail.com>
2020-07-08 09:09:43 +1200

    second commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send('Helloooo00000');
+    res.send('<h1>Helloooo</h1>');
 });
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
